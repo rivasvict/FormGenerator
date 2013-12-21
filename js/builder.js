aea8a4246c02925ea19d38ea3d47d2488d7ce260
Victor Rivas <vrivas@localhost.localdomain>
2013-12-21 18:13:31 -0500

    First html automatic creation

--- js/builder.js
+++ js/builder.js
@@ -1,6 +1,24 @@
 angular.module('fc', ['ngRoute']);
 
-.config(function($routeProvider){
-	$routeProvider
-	.when('/', )
-})
+function genCTRL($scope){
+
+	$scope.controls=[];
+
+	$scope.add = function(){
+		$scope.controls.push({text:'',delet:'false',typeC:''});
+	};
+
+	$scope.del = function(element){
+		var oldcontrols = $scope.controls;
+		$scope.controls = [];
+		element.delet = 'true';
+		angular.forEach(oldcontrols, function(element){
+			if(element.delet == 'false') $scope.controls.push(element);
+		});
+	};
+
+	$scope.test = function(){
+		alert('hola mundo');
+	};
+
+}
