e6bbbe2e87adf408f82a97f6e6681bcbe7f86a1c
Victor Rivas <vrivas@localhost.localdomain>
2013-12-25 19:53:28 -0500

    Fixing adding and deleting selects functions

--- js/builder.js
+++ js/builder.js
@@ -3,9 +3,10 @@ angular.module('fc', ['ngRoute']);
 function genCTRL($scope){
 
 	$scope.controls=[];
+	$scope.sch=[];
 
 	$scope.add = function(){
-		$scope.controls.push({text:'',delet:'false',typeC:''});
+		$scope.controls.push({text:'',delet:'false',typeC:'',select:$scope.sObject = []});
 	};
 
 	$scope.del = function(element){
@@ -17,8 +18,28 @@ function genCTRL($scope){
 		});
 	};
 
-	$scope.test = function(){
-		alert('hola mundo');
+	$scope.addSel = function(element){
+		element.select.push({text:'',delet:'false',done:'true'});
+	};
+  
+	$scope.delSel = function(selec,element){
+		var oldsObject = element.select;
+		element.select = [];
+		selec.delet = 'true';
+		angular.forEach(oldsObject, function(selec){
+			if(selec.delet == 'false') element.select.push(selec);
+                  });
+          };
+
+
+	$scope.scheme = function(element){
+		if(element.typeC == 'text'){
+			$scope.sch.push({tag:'<input type="'+element.typeC+'" name="'+element.text+'">',});
+		}else{
+			$scope.addSel(element);
+			$scope.sch.push({tag:'<select name="'+element.text+'">'});
+			$scope.sch.push({tag:'</select>'});
+		}
 	};
 
 }
