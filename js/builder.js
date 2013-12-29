5f216beb23c34226401de00a755fd4fb3575d991
Victor Rivas <vctr90w@gmail.com>
2013-12-29 08:11:14 -0500

    building the tag object for html genration

--- js/builder.js
+++ js/builder.js
@@ -3,10 +3,10 @@ angular.module('fc', ['ngRoute']);
 function genCTRL($scope){
 
 	$scope.controls=[];
-	$scope.sch=[];
+	//$scope.sch=[];
 
-	$scope.add = function(){
-		$scope.controls.push({text:'',delet:'false',typeC:'',select:$scope.sObject = []});
+	$scope.add = function(element){
+		$scope.controls.push({text:'',delet:'false',typeC:'',select:$scope.sObject = [],tag:$scope.sch = []});
 	};
 
 	$scope.del = function(element){
@@ -20,6 +20,9 @@ function genCTRL($scope){
 
 	$scope.addSel = function(element){
 		element.select.push({text:'',delet:'false',done:'true'});
+		angular.forEach(element.select, function(th){
+			element.tag.push({tag:'<option name:"'+th.text+'">'+th.text+'</option>'});
+		});
 	};
   
 	$scope.delSel = function(selec,element){
@@ -34,11 +37,13 @@ function genCTRL($scope){
 
 	$scope.scheme = function(element){
 		if(element.typeC == 'text'){
-			$scope.sch.push({tag:'<input type="'+element.typeC+'" name="'+element.text+'">',});
+			element.tag = [{}];
+			element.tag = [{tag:'<input type="'+element.typeC+'" name="'+element.text+'">'}];
 		}else{
+			element.tag = [{}];console.log(element);
+			element.tag = [{tag:'<select name="'+element.text+'">'}];
 			$scope.addSel(element);
-			$scope.sch.push({tag:'<select name="'+element.text+'">'});
-			$scope.sch.push({tag:'</select>'});
+			element.tag.push({tag:'</select>'});
 		}
 	};
 
